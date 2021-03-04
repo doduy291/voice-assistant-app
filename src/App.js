a85783cd943a1cf6c36cdffc166fac1567c5d375
doduy291 <doduy291@gmail.com>
2021-03-04 23:37:30 +0700

    Add search category,term,open news web, go back code

--- src/App.js
+++ src/App.js
@@ -1,21 +1,41 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
+import wordsToNumbers from 'words-to-numbers';
 import alanBtn from '@alan-ai/alan-sdk-web';
-const alanKey =
-  '57a22f58de6c18406cb6977e82dbd4302e956eca572e1d8b807a3e2338fdd0dc/stage';
+
+import NewsCards from './components/NewsCards/NewsCards.js';
+
+const alanKey = '57a22f58de6c18406cb6977e82dbd4302e956eca572e1d8b807a3e2338fdd0dc/stage';
+
 const App = () => {
+  const [newsArticles, setNewsArticles] = useState([]);
   useEffect(() => {
     alanBtn({
       key: alanKey,
-      onCommand: ({ command, articles }) => {
+      onCommand: ({ command, articles, number }) => {
         if (command === 'newHeadlines') {
-          console.log(articles);
+          setNewsArticles(articles);
+        } else if (command === 'open') {
+          console.log(number);
+          const parsedNumber = number.length > 2 ? wordsToNumbers(number, { fuzzy: true }) : number;
+          const article = articles[parsedNumber - 1];
+          if (parsedNumber > articles.length) {
+            alanBtn().playText('Please try that again');
+          } else if (article) {
+            window.open(article.url, '_blank');
+            alanBtn().playText('Opening...');
+          } else {
+            alanBtn().playText('Please try that again...');
+          }
         }
       },
     });
   }, []);
   return (
     <div>
-      <h1>ALAN AI News App</h1>
+      <div className="logoContainer">
+        <img src="https://voicebot.ai/wp-content/uploads/2019/10/alan.jpg" className="alanLogo" alt="logo" />
+      </div>
+      <NewsCards articles={newsArticles} />
     </div>
   );
 };
